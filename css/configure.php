3cff9a701e6ba44d387d5e66c162f3fe1804072c
Nicolas Depetri <Nicolas@Server.server>
2020-04-06 15:50:28 -0300

    Initial commit

---
+++ css/configure.php
@@ -0,0 +1,34 @@
+<?php
+  define('HTTP_SERVER', 'http://littledreams.ferozo.net');
+  define('HTTPS_SERVER', 'http://littledreams.ferozo.net');
+  define('ENABLE_SSL', false);
+  define('HTTP_COOKIE_DOMAIN', '');
+  define('HTTPS_COOKIE_DOMAIN', '');
+  define('HTTP_COOKIE_PATH', '/tienda/admin');
+  define('HTTPS_COOKIE_PATH', '/tienda/admin');
+  define('HTTP_CATALOG_SERVER', 'http://littledreams.ferozo.net');
+  define('HTTPS_CATALOG_SERVER', 'http://littledreams.ferozo.net');
+  define('ENABLE_SSL_CATALOG', 'false');
+  define('DIR_FS_DOCUMENT_ROOT', 'G:/FerozoWebHosting/littledreams.com.ar/public_html/tienda/');
+  define('DIR_WS_ADMIN', '/tienda/admin/');
+  define('DIR_WS_HTTPS_ADMIN', '/tienda/admin/');
+  define('DIR_FS_ADMIN', 'G:/FerozoWebHosting/littledreams.com.ar/public_html/tienda/admin/');
+  define('DIR_WS_CATALOG', '/tienda/');
+  define('DIR_WS_HTTPS_CATALOG', '/tienda/');
+  define('DIR_FS_CATALOG', 'G:/FerozoWebHosting/littledreams.com.ar/public_html/tienda/');
+  define('DIR_WS_CATALOG_IMAGES', DIR_WS_CATALOG . 'images/');
+  define('DIR_WS_CATALOG_LANGUAGES', DIR_WS_CATALOG . 'includes/languages/');
+  define('DIR_FS_CATALOG_LANGUAGES', DIR_FS_CATALOG . 'includes/languages/');
+  define('DIR_FS_CATALOG_IMAGES', DIR_FS_CATALOG . 'images/');
+  define('DIR_FS_CATALOG_MODULES', DIR_FS_CATALOG . 'includes/modules/');
+  define('DIR_FS_BACKUP', DIR_FS_ADMIN . 'backups/');
+  define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
+  define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
+
+  define('DB_SERVER', 'localhost');
+  define('DB_SERVER_USERNAME', 'littledreams');
+  define('DB_SERVER_PASSWORD', 'Design123');
+  define('DB_DATABASE', 'littledreams_oc');
+  define('USE_PCONNECT', 'false');
+  define('STORE_SESSIONS', 'mysql');
+  define('CFG_TIME_ZONE', 'America/Argentina/Buenos_Aires');
